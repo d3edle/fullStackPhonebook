b00c729cd0028df3ad16c76d679d1263bdcf2a46
d3edle <danp942942@gmail.com>
2025-07-17 20:36:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+// const cors = require('cors')
+
+// app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`
+    <div>Phonebook has info for ${persons.length} people</div>
+    <div>${date}</div>
+    `)
+    response.send
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number ) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  nameArr = persons.map(person => person.name)
+
+  if((nameArr.find((name) => name === body.name)) !== undefined){
+      return response.status(400).json({ 
+      error: 'duplicate name' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random()*100000),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
